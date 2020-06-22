3fde3ec4de1568bc8f4636f27a41550f0495cfa5
Soham Dongargaonkar <soham.dongargaonkar@motorolasolutions.com>
2020-06-21 22:37:30 -0400

    Add Jekyll static outputs for first blog post

---
+++ README.md
@@ -0,0 +1,5 @@
+# Site
+
+Contains source for my [website](http://a3y3.dev). The website is served by an AWS EC-2 instance. 
+
+This repo facilitates easy development by allowing me to develop on my local machine, pushing to this repository, and simply pulling it on my AWS machine.
---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const THEME = "theme";
+const THEME_LIGHT = "light";
+const THEME_DARK = "dark";
+const DATA_THEME = "data-theme";
+
+function handleTheme(){
+	//Handle theme preferences
+	let themePreference = Cookies.get(THEME);
+	
+	if(!themePreference) {
+		themePreference = THEME_LIGHT; 	// Default is light in body.
+		Cookies.set(THEME, THEME_LIGHT, {'expires': 365});
+	}
+	else {
+		let switcher = document.querySelector("#theme-switch");
+		if(themePreference === THEME_LIGHT){
+			switcher.setAttribute("checked", "checked");
+		}
+		else{
+			switcher.removeAttribute("checked");
+		}
+	}
+	document.body.setAttribute(DATA_THEME, themePreference);
+}
+handleTheme();
+
+$(function(){
+	$("#theme-switch").on("click", function(){
+		let chosenTheme;
+		if (this.checked) { chosenTheme = THEME_LIGHT; }
+		else{ chosenTheme = THEME_DARK; }
+		
+		Cookies.set(THEME, chosenTheme, {'expires': 365});
+		handleTheme();
+	});	
+});
